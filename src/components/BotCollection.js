d2465c5a3ea9d55317678c1dc7cc1d674a0edc0a
Noahkimtai <noahkimtai24@gmail.com>
2022-10-05 13:14:33 +0300

    BotCollection component refactored

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -6,17 +6,6 @@ import YourBotArmy from "./YourBotArmy";
 function BotCollection({bots}) {
   // Your code here
   // for every bot load the BotCard and the BotSpec component
-
-/*   //create a state to hold enlisted bots
-  const [enlistedBots, setEnlistedBots] = useState([])
-  function addToArmy(bot){
-    const newEnlist =[...enlistedBots,bot]
-    setEnlistedBots(newEnlist)
-  }
-  // when the enlistedBots changes add a botArmy card
-  useEffect(() =>{
-    enlistedBots.map((bot) =>{return(<YourBotArmy bot ={bot} />)})
-  },[enlistedBots]) */
   return (
     <div className="ui four column grid">
       <div className="row">
